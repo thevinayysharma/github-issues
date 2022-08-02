bae5c1cb9e2cd471c695e8f7fe8b208e8c31b398
thevinayysharma <vinay02856@gmail.com>
2022-08-02 23:28:51 +0530

    Chore: refactored syntax

--- src/components/IssueList/index.jsx
+++ src/components/IssueList/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import useFetch from '../../hooks/useFetch';
 import { useState, useEffect , useMemo } from 'react';
 import IssueBox from '../IssueBox';
-import "./style.css";
-import Pagination from "../Pagination/pagination";
+import './style.css';
+import Pagination from '../Pagination/pagination';
 
 let PageSize = 10;
 let bool = true;
@@ -13,7 +13,6 @@ function IssueList() {;
   const [issues, setIssues] = useState(data);
   const [openPullRequest, setOpenPullRequest] = useState(null);
   const [closedPullRequestCount, setClosedPullRequestCount] = useState(null);
-
 
   const currentPageData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize ;
@@ -26,6 +25,8 @@ function IssueList() {;
     setIssues((issues) => [...issues, ...data]);
   }, [data]);
 
+
+  //useEffect for updating open-closed issues
   useEffect(() => {
     const calculateOpenClosePullRequest = () => {
       let open = 0, close = 0;
@@ -45,10 +46,10 @@ function IssueList() {;
         <p className="header__tittle">👋 Want to contribute to facebook/react?</p>
         <p className="header__para">If you have a bug or an idea, read the <a href="https://github.com/facebook/react/blob/9fcaf88d58cfd942e2fdd303ae8291dbf4828969/CONTRIBUTING.md"> contributing guidelines </a>before opening an issue.</p>
         <p className="header__para">If you're ready to tackle some open issues, <a href="https://github.com/facebook/react/contribute">we've collected some good first issues for you.</a></p>
-       </header>
+      </header>
      <div className="IssueList">
       <div className="error" >{error && <strong>{error}</strong>}</div>
-       <div className='IssueList__header'>
+       <div className="IssueList__header">
         <span className="IssueList__header__state--open">{openPullRequest ?? 0} Open</span>
         <span className="IssueList__header__state--closed"> ✓ {closedPullRequestCount ?? 0} Closed</span>
       </div>
